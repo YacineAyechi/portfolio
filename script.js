e85aee1e9541c7cc396acf810d8cb9dd0f360cdf
YacineAyechi <70200937+YacineAyechi@users.noreply.github.com>
2023-03-29 13:49:32 +0100

    new changes

--- script.js
+++ script.js
@@ -1,47 +1,7 @@
-// // Select all images in the grid
-// const images = document.querySelectorAll(".image-grid img");
-
-// // Add a click event listener to each image
-// images.forEach((img) => {
-//   img.addEventListener("click", () => {
-//     // Open the image in a new tab when clicked
-//     window.open(img.src, "_blank");
-//   });
-// });
-
 // Select the current year and insert it into the footer
 const yearSpan = document.getElementById("year");
 const currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
-
-const form = document.getElementById("contact-form");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  // Get form values
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-
-  // Send email using email.js
-  emailjs
-    .send("service_kfq6nye", "template_h7s70ve", {
-      from_name: name,
-      from_email: email,
-      message: message,
-    })
-    .then(
-      (response) => {
-        alert("Your message has been sent successfully!");
-        form.reset();
-      },
-      (error) => {
-        alert("Oops! An error occurred, please try again.");
-        console.log(error);
-      }
-    );
-});
 
 // Add smooth scrolling effect to navbar links
 const navbarLinks = document.querySelectorAll('nav a[href^="#"]');
